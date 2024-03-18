74426ef7a584a6607b1ddecd9d9c2c63003259a4
Abhishek Yadav <abhishekay442080@gmail.com>
2024-03-18 13:37:20 +0530

    Changing in header.js file

--- app/components/Header.js
+++ app/components/Header.js
@@ -20,25 +20,11 @@ const Header = () => {
                     <span className="ml-3 text-xl">Aby Traders</span>
                 </a>
                 <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
-                    <a className="mr-5 hover:text-gray-900">First Link</a>
-                    <a className="mr-5 hover:text-gray-900">Second Link</a>
-                    <a className="mr-5 hover:text-gray-900">Third Link</a>
-                    <a className="mr-5 hover:text-gray-900">Fourth Link</a>
+                    <a className="mr-5 hover:text-gray-900">Home</a>
+                    <a className="mr-5 hover:text-gray-900">About Us</a>
+                    <a className="mr-5 hover:text-gray-900">Contact US</a>
+                    <a className="mr-5 hover:text-gray-900">Blogs</a>
                 </nav>
-                {/* <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
-                    Button
-                    <svg
-                        fill="none"
-                        stroke="currentColor"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        className="w-4 h-4 ml-1"
-                        viewBox="0 0 24 24"
-                    >
-                        <path d="M5 12h14M12 5l7 7-7 7"></path>
-                    </svg>
-                </button> */}
             </div>
         </header>
     );
